96a4d19fd14901bbc6f7ec34f2520f6c00139d26
A Torin <alberto.torin@gmail.com>
2018-05-27 22:01:57 +0200

    Aggiunte conversioni inverse
    Aggiunti i file ottenuti dalla conversione inversa da un formato X
    verso RST.
    Si possono notare alcune differenze negli output, forse dovute a
    come pandoc tratta i diversi formati.
    Potrebbe essere interessante analizzare da dove vengono queste
    differenze.

---
+++ all2rst.sh
@@ -0,0 +1,4 @@
+pandoc esempio.html -o inverse/esempio-html.rst
+pandoc esempio.docx -s -o inverse/esempio-docx.rst
+pandoc esempio.odt -o inverse/esempio-odt.rst
+pandoc esempio.md -o inverse/esempio-md.rst
